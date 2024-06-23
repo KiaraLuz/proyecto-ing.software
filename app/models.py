122eb3a44b00e2646766efc2926c18848e5e1d3d
Kenner66 <kennerechevarria@gmail.com>
2024-06-23 16:31:38 -0500

    Usuario_model_forms

--- app/models.py
+++ app/models.py
@@ -6,3 +6,7 @@ class Rol(models.Model):
     nombre_rol = models.CharField(max_length=50)
     descripcion = models.TextField(blank=True)
     estado = models.BooleanField(default=True)
+class Usuario(models.Model):
+    id_usuario=models.CharField(max_length=15, primary_key=True)
+    nombre_usuario = models.CharField(max_length=50)
+    rol_usuario = models.ForeignKey(Rol, on_delete=models.CASCADE)
