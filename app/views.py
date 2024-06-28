d4044e3ae854184cd667badd8c49141ae38b91d9
Roberto Eustaquio Jiménez <reusjimenez2002@gmail.com>
2024-06-28 11:19:19 -0500

    views Ingrediente

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from app.models import Rol, Usuario
-from app.forms import RolForm, UsuarioForm
+from app.models import Rol, Usuario, Ingrediente
+from app.forms import RolForm, UsuarioForm, IngredienteForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
@@ -93,3 +93,34 @@ def usuario_modificar(request, usuario_id):
         form = UsuarioForm(instance=usuario)
     contexto = {'form': form}
     return render(request, "usuario/usuario_modificar.html", contexto)
+
+@login_required
+def ingredientes(request):
+    ingredientes = Ingrediente.objects.all()
+    contexto = {'ingredientes': ingredientes}
+    return render(request, "ingrediente/ingrediente.html", contexto)
+
+@login_required
+def ingrediente_crear(request):
+    if request.method == "POST":
+        form = IngredienteForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('ingredientes')
+    else:
+        form = IngredienteForm()
+    contexto = {'form': form}
+    return render(request, "ingrediente/ingrediente_crear.html", contexto)
+
+@login_required
+def ingrediente_modificar(request, ingrediente_id):
+    ingrediente = get_object_or_404(Ingrediente, id_ingrediente=ingrediente_id)
+    if request.method == "POST":
+        form = IngredienteForm(request.POST, instance=ingrediente)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('ingredientes')
+    else:
+        form = IngredienteForm(instance=ingrediente)
+    contexto = {'form': form}
+    return render(request, "ingrediente/ingrediente_modificar.html", contexto)
