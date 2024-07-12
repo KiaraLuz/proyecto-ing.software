eb5f05d4a3354778e0c15653f9c96058176f40ac
Kiara <fernandezperezkiara@gmail.com>
2024-07-12 18:48:58 -0500

    tabla unidades de medida

--- app/models.py
+++ app/models.py
@@ -32,3 +32,10 @@ class Ingrediente(models.Model):
     cantidad = models.DecimalField(max_digits=10, decimal_places=2)
     unidad = models.CharField(max_length=10)
     estado_ingrediente = models.BooleanField(default=True)
+
+class UnidadesMedida(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
